1f9f08db0b9773480c20cd5f5b145946db3356a9
UsielOM <usiel31197@outlook.es>
2025-07-15 11:16:33 -0600

    Mecanica de deslizar en la pared y salto en la pared

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,16 +2,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // ===== Serialized Field =====
+    [Header("Ground Check")]
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask whatIsGround;
     [SerializeField] private float groundCheckRadius = 1f;
+
+    [Header("Wall Check")]
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask whatIsWall;
+
+    [Header("Movement")]
     [SerializeField] private float speed = 3f;
+
+    [Header("Jump")]
     [SerializeField] private float jumpForce = 5f;
     [SerializeField] private int extraJumps = 1;
     [SerializeField] private float extraJumpForce = 2f;
     [SerializeField] private float coyoteTime = 0.1f;
     [SerializeField] private float jumpBufferTime = 0.1f;// Mejora la experiencia de salto apra el usuario 
 
+    [Header("Wall Slide")]
+    [SerializeField] private float wallSlideSpeed = 0.5f;
+
+    [Header("Wall Jump")]
+    [SerializeField] private Vector2 wallJumpForce = new Vector2(5f, 8f);
+    [SerializeField] private float wallJumpDuration;
+
+
+    // =====  Properties privates =====
     private float x = 0f;
     private bool jumpPressed;
     private int currentExtraJumps;
@@ -21,59 +40,73 @@ public class PlayerController : MonoBehaviour
     private float jumpBufferCounter;
     private bool inJumpBuffer => jumpBufferCounter > 0;//Getter  este tendra el valor envace al resultado de la funcion flecha 
 
-
-
     private Rigidbody2D rb;
     private Animator anim;
-   
+
+    private bool slidingWall;
+    private bool wallJumping;
+
+
+    // =====  Unity Events =====
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
 
-    void Start()
+     void Start()
     {
         
     }
 
      void FixedUpdate()//Motor de fisicas
     {
-        rb.linearVelocity = new Vector2(x * speed, rb.linearVelocity.y);
+        if(!wallJumping)
+        {
+            rb.linearVelocity = new Vector2(x * speed, slidingWall ? -wallSlideSpeed : rb.linearVelocity.y);
+        }
+        
     }
 
-     void OnDrawGizmos()//al usar gismos // se va a dinuajr una esfera 
+     void OnDrawGizmos()//al usar gismos // se va a dibujar una esfera 
     {
-        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);// En este punto se dibujo la esfera 
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);// En este punto se dibujo la esfera
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(wallCheck.position, wallCheck.localScale);
     }
 
-    // Update is called once per frame
     void Update()
     {
         DetectInput();
         JumpPlayer();
+        HandelWallSlide();
         FlipPlayer();
         UpdateAnimatorParameters();
     }
 
-
+    // ===== Functions Private =====
     private void DetectInput()
     {
-        x = Input.GetAxis("Horizontal");
+        x = Input.GetAxisRaw("Horizontal");// devuelve el valor en crudso en valores enteros
         jumpPressed = Input.GetButtonDown("Jump");
     }
 
-
     private void FlipPlayer()//flipep mediante la posision
     {
-        Vector3 scale = transform.localScale;
-        if(x > 0)
+        if (!wallJumping)
         {
-            transform.localScale = new Vector3(Mathf.Abs(scale.x), scale.y, scale.z);//Se hace uso de valor absoluto 
-        } else if (x < 0) 
-        {
-            transform.localScale = new Vector3(-Mathf.Abs(scale.x), scale.y, scale.z);// se puso un menos para tranformar la escal a anegativo sea el valor que sea
+            Vector3 scale = transform.localScale;
+            if (x > 0)
+            {
+                transform.localScale = new Vector3(Mathf.Abs(scale.x), scale.y, scale.z);//Se hace uso de valor absoluto 
+            }
+            else if (x < 0)
+            {
+                transform.localScale = new Vector3(-Mathf.Abs(scale.x), scale.y, scale.z);// se puso un menos para tranformar la escal a anegativo sea el valor que sea
+            }
         }
+  
     }
 
     private void JumpPlayer () // salto mediante addForce 
@@ -85,9 +118,10 @@ public class PlayerController : MonoBehaviour
         {
             jumpBufferCounter -= Time.deltaTime;
         }
+
         if(inJumpBuffer)
         {
-            if (inCoyoteTime || currentExtraJumps < extraJumps)
+            if ((inCoyoteTime || currentExtraJumps < extraJumps) && !slidingWall)
             {
                 rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
                 rb.AddForce(Vector2.up * (inCoyoteTime ? jumpForce : extraJumpForce), ForceMode2D.Impulse);
@@ -96,12 +130,14 @@ public class PlayerController : MonoBehaviour
                     currentExtraJumps++;//COYOTE TIME PERMITE SALTAR MIENTRAS ESTAS EN AL AIRE POR UNOS SEGUNDOS PEQUEÑOS 
                     anim.SetTrigger("dobleJump");
                 } else anim.SetTrigger("Jump");
-                
+
                 coyoteTimeCounter = 0;
                 jumpBufferCounter = 0;
-               
+            } else if (slidingWall) {
+                WallJump();
             }
         }
+
         if (IsGrounded() && rb.linearVelocity.y < 0.1f)
         {
             currentExtraJumps = 0;
@@ -111,20 +147,50 @@ public class PlayerController : MonoBehaviour
         {
             coyoteTimeCounter -= Time.deltaTime;
         }
-       
     }
 
+    private void WallJump()
+    {
+        wallJumping = true;
+        int direction = transform.localScale.x > 0 ? -1 : 1;// esto nos va a permitir saltar a la direccion que corresponde 
+        rb.linearVelocity = new Vector2(wallJumpForce.x * direction, wallJumpForce.y);// sE ESTA DANDO LA FUERZA DEL SALTO A LA DIRECCION QUE ESTE VIENDO
+        currentExtraJumps = 0;
+
+       
+        Invoke(nameof(StopWallJump), wallJumpDuration);//invoked va ejecutar una fucnion despeus de cierto tiempo que lo indiquemos
+
+        Vector3 scale = transform.localScale;
+        transform.localScale = new Vector3(direction * -Mathf.Abs(scale.x), scale.y, scale.z);// Esto va a rotar al personaje  a la direccion del salto
+
+        coyoteTimeCounter = 0;
+        jumpBufferCounter = 0;
+    }
+
+    private void StopWallJump() { 
+        wallJumping = false;
+    }
+    private void HandelWallSlide() {
+        if (IsWalled() && !IsGrounded() && x != 0 && rb.linearVelocity.y < -0.1f) {
+            slidingWall = true;
+        } else  slidingWall = false;
+    }
     private bool IsGrounded ()
     {
         bool isGroundedPlayer = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);//Este va a dectectar si la esfera esta tocando la capa que le indiquemos se pasa la referencia de la esfera
         return isGroundedPlayer;
     }
 
+    private bool IsWalled ()
+    {
+        bool isWalled = Physics2D.OverlapBox(wallCheck.position, wallCheck.localScale, 0, whatIsWall);
+        return isWalled;
+    }
 
     private void UpdateAnimatorParameters()
     {
         anim.SetFloat("HorVelocity", Mathf.Abs(rb.linearVelocity.x));// seteamos el valor de  velocidad de x 
         anim.SetFloat("VerVelocity", rb.linearVelocity.y);// seteamos el valor de  velocidad de y
         anim.SetBool("isGrounded", IsGrounded());//seteamos el valor falso o verdadero
+        anim.SetBool("wallJump", slidingWall);
     }
 }
