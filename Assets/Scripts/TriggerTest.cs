9f2f5bcb94531967a689dbd28151928079246a3c
UsielOM <usiel31197@outlook.es>
2025-07-16 19:19:27 -0600

    Mi segundo trigger

--- Assets/Scripts/TriggerTest.cs
+++ Assets/Scripts/TriggerTest.cs
@@ -6,20 +6,9 @@ public class TriggerTest : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision) // Parametro de entrada
     {
-        //cuando detecte que algo entre al trigger puede ejecutar este codigo
-
-        //print(collision.gameObject.name);// estamos obteniendo lo que entro en el trigger
-
-    }
-
-     void OnTriggerExit2D(Collider2D collision)// cuando algo sale del trigger
-    {
-       /// print(collision.gameObject.name); // lo que sale del colicionador
-    }
-
-      void OnTriggerStay2D(Collider2D collision)// Aqui genera un bucle constante mientras este dentro del trigger el gameObject  solo si se mueve
-    {
-        //print(collision.gameObject.name);
+        if(collision.gameObject.CompareTag("Player")) {
+            print("Ha entrado el jugador");
+        }
     }
 
 }
