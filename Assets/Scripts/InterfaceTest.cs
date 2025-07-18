193a7df2e35636b1ba0d2e3e4c4a602a3e24df6d
UsielOM <usiel31197@outlook.es>
2025-07-17 19:41:26 -0600

    Elementos intreactivos

--- Assets/Scripts/InterfaceTest.cs
+++ Assets/Scripts/InterfaceTest.cs
@@ -15,4 +15,30 @@ public class InterfaceTest : MonoBehaviour
     {
         
     }
+
+
+    public void SayHello()
+    {
+        print("Hello World");
+    }
+
+    public void ActivateSomeThing(bool isOn)
+    {
+        print($"El estado del toggel es: {isOn}");
+    }
+
+    public void ChangedVolume(float sliderValue)
+    {
+        print($"El volumen a cambhiado a : {sliderValue}");
+    }
+
+    public void ChangedName(string text)
+    {
+        print($"El texto ha cambiado a: {text}");
+    }
+
+    public void OnEndEdit(string text)
+    {
+        print($"El texto final es: {text}");
+    }
 }
